adca264bb4c07cbda0fc56fdf04180c05ae26cff
Nick Gill <GILL@Gills-MacBook-Pro.local>
2015-11-22 22:41:58 -0600

    finshed challenge

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,34 +1,75 @@
 $( function() {
 
-//Load the data for the application
-    $.ajax( {
-           url: "data/eta.json"
-       } )
-       .done( function( json ) {
-           console.log( json );
-           var object = JSON.parse( json );
-           console.log( object );
-       } );
+    var slidePosition = 0;
+    var currentDataSet = null;
 
-// Push firstName and lastName into the html to be displayed on .displayName h3 tag
-       var students = function() {
+    $( ".previous" ).on( "click", function( event ) {
+            event.preventDefault();
+            slidePosition--;
+            if ( slidePosition < 0 ) {
+                slidePosition = 0;
+                reachedEnd( "beginning" );
+            }
+            loadData();
+    } );
 
-       };
+    // On click button for next and previous
+    $( ".next" ).on( "click", function( event ) {
+        event.preventDefault();
+        slidePosition++;
+        if ( slidePosition > currentDataSet.length - 1 ) {
+            slidePosition = currentDataSet.length - 1;
+            reachedEnd( "end " );
+        }
+        loadData();
+    } );
 
-// On next button make it go to the next person
+    //Load the data for the application
+    var loadData = function() {
+        $.ajax( {
+            url: "data/eta.json"
+        } )
+        .done( function( json ) {
+            var item = getNextItem( slidePosition, json.eta );
+            getMarkupName( item );
+            getMarkupMovies( item );
+            currentDataSet = json.eta;
+            console.log( json.eta );
+        } );
+    };
+    loadData();
 
+    // Push firstName and lastName into the html to be displayed on .displayName h3 tag
+    var getNextItem = function( index, data ) {
+        return data[ index ];
+    };
 
-// On previous button make current student cycle the through names in reverse
+    var getMarkupName = function( data ) {
+        var source = $( ".nameTemplate" ).html();
+        var template = Handlebars.compile( source );
+        var html = template( data );
+        updateDom( html );
+    };
 
-// Each time the next or previous button is selected show favoriteMovies
+    var getMarkupMovies = function( data ) {
+        var source = $( ".moviesTemplate" ).html();
+        var template = Handlebars.compile( source );
+        var html = template( data );
+        updateMovie( html );
 
-// On click button for next and previous
-    $('.next').on('click' function() {
+    };
 
-    });
+    var updateDom = function( html ) {
+        $( ".displayName" ).fadeOut();
+        $( ".displayName" ).html( html );
+        $( ".displayName" ).fadeIn();
+    };
 
-    $('.previous').on('click' function() {
-
-    });
+    var updateMovie = function( html ) {
+        $( ".movies" ).html( html );
+    };
+    var reachedEnd = function( whichend ) {
+        alert( "You have reached the " + whichend );
+    };
 
 } );
